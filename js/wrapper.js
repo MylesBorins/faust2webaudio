003bac08f003f6aed8d55575fd5710999fb779b8
Myles Borins <myles.borins@gmail.com>
2013-12-17 13:38:41 -0500

    Add dsp.connect()
    This method allows you to connect a dsp module to another webaudio node

--- js/wrapper.js
+++ js/wrapper.js
@@ -63,6 +63,11 @@ var faust = faust || {};
         that.destroy = function () {
             DSP_destructor(that.ptr);
         };
+        
+        // Connect to another node
+        that.connect = function (node) {
+          that.jsNode.connect(node);
+        }
 
         // Bind to Web Audio
 
