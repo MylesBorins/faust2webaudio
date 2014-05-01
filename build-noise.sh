cd99379b754ef416f30992f2a428bc7629dc5b92
Myles Borins <myles@famo.us>
2014-05-01 12:12:53 +0200

    Update build scripts to optimize

--- build-noise.sh
+++ build-noise.sh
@@ -10,7 +10,7 @@ echo "Wrapping dat cpp"
 sed -e "s/DSP/NOISE/g" -e "s/Dsp/Noise/g" -e "s/dsp/noise/g" cpp/faust-wrapper.cpp >> cpp/faust-noise.cpp
 echo " $(tput setaf 2)Complete$(tput sgr0)"
 echo "Using emscripten to compile to JavaScript"
-emcc cpp/faust-noise.cpp -o js/faust-noise-temp.js \
+emcc -O2 cpp/faust-noise.cpp -o js/faust-noise-temp.js \
 -s EXPORTED_FUNCTIONS="['_NOISE_constructor','_NOISE_destructor','_NOISE_compute', '_NOISE_getNumInputs', '_NOISE_getNumOutputs', '_NOISE_getNumParams', '_NOISE_getNextParam']"
 echo " $(tput setaf 2)Complete$(tput sgr0)"
 echo "Wrapping js"
